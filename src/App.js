5d35892b74b239f2f16f0185ece853de5dd262d0
Adib Firman <dib.firman@gmail.com>
2018-11-23 16:42:04 +0700

    linking bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <h2 className="text-center p2">Simple Todo APP</h2>
+        </div>
+        <div className="container">
+          <ul className="list-group">
+            <li className="list-group-item">siapp</li>
+          </ul>
+        </div>
       </div>
     );
   }
+
 }
 
 export default App;
